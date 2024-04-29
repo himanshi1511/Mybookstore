93852dde530ce1094ae2672c8e0c64f9b111ab69
Himanshi <himanshi0609.be21@chitkara.edu.in>
2024-04-29 22:38:03 +0530

    first commit

---
+++ backends/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+//export const mongoDBURL = 'mongodb+srv://root:himanshi1511@book-store-mern.hfnh55c.mongodb.net/?retryWrites=true&w=majority'
+
+export const mongoDBURL = 'mongodb+srv://root:himanshi1511@book-store-mern.hfnh55c.mongodb.net/?retryWrites=true&w=majority&appName=Book-store-MERN'
